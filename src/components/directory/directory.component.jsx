7d6062898b673950244c1d7fef32fa7bc0d7960b
larajunho <larinhajunho@hotmail.com>
2022-05-17 00:44:33 -0300

    * apply styled components to collection-preview and directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,16 +4,16 @@ import { createStructuredSelector } from "reselect"
 
 import { selectDirectorySections } from "../../redux/directory/directory.selectors"
 import MenuItem from "../menu-item/menu-item.compenent"
-import "./directory.styles.scss"
+import { DirectoryMenu } from "./directory.styles.jsx"
 
 const Directory = ({ sections }) => (
-	<div className="directory-menu row">
+	<DirectoryMenu className="row">
 		{sections.map(({ id, ...otherSectionProps }) => (
 			<div key={id} className="col-12 col-md-6 col-lg-4 mb-3">
 				<MenuItem {...otherSectionProps} />
 			</div>
 		))}
-	</div>
+	</DirectoryMenu>
 )
 
 const mapStateToProps = createStructuredSelector({
